489234f75ae047ee2646f5104a49ea424e82510f
mayonice1424 <friyandrailham14@gmail.com>
2025-08-14 03:46:06 +0700

    update env

--- config/db.go
+++ config/db.go
@@ -6,13 +6,35 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
 )
 
+
+var (
+	dbHost     string
+	dbPort     string
+	dbUser     string
+	dbPassword string
+	dbName     string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbHost  = os.Getenv("DB_HOST")
+	dbPort = os.Getenv("DB_PORT")
+	dbUser = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbName = os.Getenv("DB_NAME")
+}
+
 func ConnectToDatabase() *sql.DB {
-	// Ganti localhost dengan db, nama service dari Docker Compose
-	connStr := "user=postgres dbname=postgres password=polkmn1234 host=localhost port=5431 sslmode=disable"  // Menggunakan port 5432 di container
+	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", dbUser, dbName, dbPassword, dbHost, dbPort)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Database Connection Failed: ", err)
